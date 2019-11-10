865ac9a35c833bf44132017c94240a27aa2b31bd
黑色的白兔子 <592128947@qq.com>
2019-11-10 13:32:22 +0800

    完成了一部分类的创建并更新了工作表

---
+++ src/pers/bwr/translaterForPerson/lineReader/ReadLineFromShortTxt.java
@@ -0,0 +1,14 @@
+package pers.bwr.translaterForPerson.lineReader;
+
+import pers.bwr.translaterForPerson.lineReader.fac.LineReader;
+
+//从只有一句话的txt中读取的方法
+public class ReadLineFromShortTxt implements LineReader {
+
+	@Override
+	public String readLine(String dataName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
