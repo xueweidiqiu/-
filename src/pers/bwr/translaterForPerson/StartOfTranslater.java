83822a787371670bb5d7c916387244dcdf74a8dd
黑色的白兔子 <592128947@qq.com>
2019-11-11 18:05:45 +0800

    完成了从只有一行的txt中读取内容的代码

--- src/pers/bwr/translaterForPerson/StartOfTranslater.java
+++ src/pers/bwr/translaterForPerson/StartOfTranslater.java
@@ -1,10 +1,24 @@
 package pers.bwr.translaterForPerson;
 
+import java.io.IOException;
+
+import pers.bwr.translaterForPerson.lineReader.ReadLineFromShortTxt;
+
 public class StartOfTranslater {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//对git流程的测试性修改
+		//正式代码
+		
+		//测试用代码
+		//测试从txt中读取一行的代码
+		try {
+			String txtLine = new ReadLineFromShortTxt().readLine("oneLine.txt");
+			System.out.println(txtLine);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		///测试从txt中读取一行的代码
 	}
 
 }
