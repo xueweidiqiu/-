a0afcf0074aa9e9fe4dbe50b878b8b18aa83ead6
黑色的白兔子 <592128947@qq.com>
2019-12-15 14:02:22 +0800

    修改了类名的错误

--- src/pers/bwr/translaterForPerson/translater/YoudaoFanyi.java
+++ src/pers/bwr/translaterForPerson/translater/YoudaoFanyi.java
@@ -2,7 +2,7 @@ package pers.bwr.translaterForPerson.translater;
 
 import pers.bwr.translaterForPerson.translater.fat.Translater;
 
-public class YouDakFanyi implements Translater {
+public class YoudaoFanyi implements Translater {
 
 	@Override
 	public String translateLine(String translateThem) {
